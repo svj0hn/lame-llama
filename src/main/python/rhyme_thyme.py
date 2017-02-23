518eaeb75bee93e83bb7d06995254afe8a5a52dc
Max Astrand <max.astrand@se.abb.com>
2017-02-23 15:16:00 +0100

    Refactor rhymethyme for easier unit testing

--- src/main/python/rhyme_thyme.py
+++ src/main/python/rhyme_thyme.py
@@ -1,16 +1,25 @@
 class RhymeThyme(object):
 
-    def rhyme_thyme(self):
-        #!/usr/bin/env python
-        
+    def rhyme_thyme(self, word):
         # Init -- load corpus etc.
-        
-        # Input word
-        #word = input('Input word!')
-
         # Look for rhyming word
 
         # Output rhyming word
-
-        #print('Thyme')
         return 'Thyme\n'
+
+    def input_from_user(self):
+        # Input word
+        word = input('Input word:   ')
+        return word
+
+    def output_to_user(self, word):
+        # Output word to user
+        print('Rhyming word:  ' + word)
+
+    def __init__(self):
+        word = self.input_from_user()
+        rhyming_word = self.rhyme_thyme(word)
+        self.output_to_user(rhyming_word)
+
+if __name__ == '__main__':
+    RhymeThyme()
--- src/unittest/python/rhyme_thyme_tests.py
+++ src/unittest/python/rhyme_thyme_tests.py
@@ -5,41 +5,5 @@ from rhyme_thyme import RhymeThyme
 class test_rhyme_thyme(unittest.TestCase):
 
     def test_output(self):
-        #self.assertEqual(RhymeThyme.rhyme_thyme(self), 'Thyme\n', 'Not equal!')
-        self.assertEqual(RhymeThyme.rhyme_thyme(self), 'Yo mama\n', 'Not equal!')
-
-
-
-#import unittest
-
-##from rhyme_thyme import rhyme_thyme
-
-#def rhyme_thyme():
-#    return "Thyme\n"
-
-#class test_rhyme_thyme(unittest.TestCase):
-
-#    def test_output(self):
-#        self.assertEqual(rhyme_thyme(), 'Thyme\n', 'not equal ')
-#        #self.assertEqual(rhyme_thyme(), 'Yo mama', 'not equal ') # not correct behaviour
-
-
-#if __name__ == '__main__':
-#    #print(rhyme_thyme()) ## 
-    
-   
-#    unittest.main()
-	
-	
-#from mockito import mock, verify
-#import unittest
-
-#from rhyme_thyme import rhyme_thyme
-
-#class RhymeThymeTest(unittest.TestCase):
-#    def test_should_issue_default_message(self):
-#        out = mock()
-
-#        rhyme_thyme(out)
-
-#        verify(out).write("Thyme\n")
+        self.assertEqual(RhymeThyme.rhyme_thyme(self,''), 'Thyme\n', 'Not equal!')
+        #self.assertEqual(RhymeThyme.rhyme_thyme(self), 'Yo mama\n', 'Not equal!')
