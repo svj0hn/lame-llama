8d5536c8958d53429879200a425152a24aafd252
Max Astrand <max.astrand@se.abb.com>
2017-03-06 14:56:27 +0100

    Properly tested code (unfortunately sub 70% coverage). Check if it works in python 2.7 / linux.

--- src/main/python/rhyme_thyme.py
+++ src/main/python/rhyme_thyme.py
@@ -7,21 +7,26 @@ class RhymeThyme(object):
         # Look for rhyming word
 
         # Output rhyming word
-        return 'Thyme'
+        return 'Thyme\n'
 
     def input_from_user(self):
         # Input word
         version_info = sys.version_info[:2]
         using_python_v3 = version_info[0] == 3
         out_str = 'Input word:   '
-        word = 'Thyme' #input(out_str) if using_python_v3 else raw_input(out_str)
+        word = input(out_str) if using_python_v3 else raw_input(out_str)
+        return word
 
     def output_to_user(self, word):
         # Output word to user
-        print('Rhyming word:  ' + word)
+        text = 'Rhyming word:  ' + word
+        print(text)
+        return text
 
     def print_welcome_text(self):
-        print("Welcome to Rhyme Thyme - the awesome rhyming time. \n")
+        text = "Welcome to Rhyme Thyme - the awesome rhyming time. \n"
+        print(text)
+        return text
 
     def print_goodbye_text(self):
         text = "Goodbye!\n" 
--- src/unittest/python/rhyme_thyme_tests.py
+++ src/unittest/python/rhyme_thyme_tests.py
@@ -4,12 +4,18 @@ from rhyme_thyme import RhymeThyme
 
 class test_rhyme_thyme(unittest.TestCase):
 
-    def test_output(self):
-        print('in output test')
-        #self.assertEqual(self.obj.rhyme_thyme(self,''), 'Thyme\n', 'Not equal!')
-        #self.assertEqual(RhymeThyme.rhyme_thyme(self,''), 'Yo mama\n', 'Not equal!')
+    def test_rhyme_thyme(self):
+        self.assertEqual(RhymeThyme.rhyme_thyme(self,'abcd'), 'Thyme\n', 'Not returning rhyming word')
 
     def test_goodbye(self):
-        obj = RhymeThyme()
-        self.assertEqual(obj.print_goodbye_text(), 'Goodbye!\n')
+        text = 'Goodbye!\n'
+        self.assertEqual(RhymeThyme.print_goodbye_text(self), text, 'Not printing goodbye text properly!')
     
+    def test_welcome(self):
+        text = "Welcome to Rhyme Thyme - the awesome rhyming time. \n"
+        self.assertEqual(RhymeThyme.print_welcome_text(self), text, 'Not printing welcome text properly!')
+
+    def test_output_to_user(self):
+        word = 'whazzup'
+        text = 'Rhyming word:  ' + word
+        self.assertEqual(RhymeThyme.output_to_user(self, word), text, 'Not outputting to user properly!')
