0be5d1b3fcbbb40008f549c24d9c9c802dc5c715
melhua <mkokic65@gmail.com>
2017-03-09 15:30:14 +0100

    rhyming feature branch

--- src/main/python/rhyme_thyme.py
+++ src/main/python/rhyme_thyme.py
@@ -1,13 +1,23 @@
 import sys
+import nltk
 
 class RhymeThyme(object):
 
     def rhyme_thyme(self, word):
-        # Init -- load corpus etc.
-        # Look for rhyming word
-
-        # Output rhyming word
-        return 'Thyme\n'
+        entries = nltk.corpus.cmudict.entries()
+        syllables = [(inp, syl) for inp, syl in entries if inp == word]
+        level = 2
+        rhymes = []
+        for (inp, syllable) in syllables:
+            rhymes += [inp for inp, pron in entries if pron[-level:] == syllable[-level:]]
+        #answers = list(set(rhymes))[0:5] #Five answers
+        #output = []
+        #for i in range(len(answers)):
+            #output.append(str(answers[i]))
+        answers = list(set(rhymes))[0] #One answer
+        output = str(answers)
+        return output
+        #return 'Thyme\n'
 
     def input_from_user(self):
         # Input word
@@ -37,6 +47,8 @@ class RhymeThyme(object):
         self.print_welcome_text()
         word = self.input_from_user()
         rhyming_word = self.rhyme_thyme(word)
+        #for i in range(len(rhyming_word)):
+            #self.output_to_user(rhyming_word[i])
         self.output_to_user(rhyming_word)
 
 if __name__ == '__main__':
